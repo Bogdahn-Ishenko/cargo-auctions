44c553cf29f489469e8c28bc341b6aefc9901912
Bogdahn-Ishenko <ishenkobogdahn@gmail.com>
2026-07-31 11:21:28 +0300

    feat(auctions): показать активные фильтры списка

--- src/pages/AuctionsList/model/AuctionsListSearch.schema.test.ts
+++ src/pages/AuctionsList/model/AuctionsListSearch.schema.test.ts
@@ -15,19 +15,31 @@ describe("AuctionsListSearchSchema", () => {
   it("coerces numeric and boolean search params", () => {
     expect(
       AuctionsListSearchSchema.parse({
+        current_price_from: "30000",
         is_available: "true",
+        is_bidder: "true",
         is_favorite: "true",
+        load_city: "Пермь",
+        load_date_from: "2026-05-26",
         page: "2",
         per_page: "10",
         price_per_km_from: "100",
+        unload_city: "Москва",
+        unload_date_to: "2026-05-29",
         weight_to: "20",
       }),
     ).toMatchObject({
+      current_price_from: 30000,
       is_available: true,
+      is_bidder: true,
       is_favorite: true,
+      load_city: "Пермь",
+      load_date_from: "2026-05-26",
       page: 2,
       per_page: 10,
       price_per_km_from: 100,
+      unload_city: "Москва",
+      unload_date_to: "2026-05-29",
       weight_to: 20,
     })
   })

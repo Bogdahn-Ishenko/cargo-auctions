20a5e9726d1cacef4e065ee21c78c39f21ffd41e
Bogdahn-Ishenko <ishenkobogdahn@gmail.com>
2026-07-31 11:59:50 +0300

    build(project): разделить vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,18 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          query: ["@tanstack/react-query"],
+          router: ["@tanstack/react-router"],
+          ui: ["@remixicon/react", "radix-ui", "sonner"],
+          vendor: ["@hookform/resolvers", "react", "react-dom", "react-hook-form", "zod", "zustand"],
+        },
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
